227a024a208ab4f4eb8548561c3bfba7042a2c09
Aneesh-Maurya <aneesh.maurya273@gmail.com>
2025-09-28 11:11:55 +0530

    second commit

--- src/app/services/project-services.service.ts
+++ src/app/services/project-services.service.ts
@@ -5,32 +5,30 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ProjectServicesService {
 
-  
+  constructor(private http:HttpClient) { }
 
   url='https://project-management-backend-kwuy.onrender.com/api/v1/projects';
 
-  private headers: HttpHeaders;
-
-  constructor(private http: HttpClient) {
-    const token = sessionStorage.getItem('UserInfo')
-      ? JSON.parse(sessionStorage.getItem('UserInfo') || '{}').accessToken
-      : '';
-
-    this.headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-  }
-
   getUserProjects(){
-  
-    return this.http.get(this.url+'/get-all-project',{headers:this.headers})
+    const token = sessionStorage.getItem('UserInfo') ? JSON.parse(sessionStorage.getItem('UserInfo') || '{}').accessToken : '';
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get(this.url+'/get-all-project',{headers:headers})
   }
   updateProject(projectid:any,data:any){
-    return this.http.put(this.url+'/update-project/'+projectid,data,{headers:this.headers})
+   const token = sessionStorage.getItem('UserInfo') ? JSON.parse(sessionStorage.getItem('UserInfo') || '{}').accessToken : '';
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.put(this.url+'/update-project/'+projectid,data,{headers:headers})
   }
   addProject(data:any){
     
-    return this.http.post(this.url+'/create-project',data,{headers:this.headers})
+   const token = sessionStorage.getItem('UserInfo') ? JSON.parse(sessionStorage.getItem('UserInfo') || '{}').accessToken : '';
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post(this.url+'/create-project',data,{headers:headers})
   }
   deleteProject(projectid:any){
-    return this.http.delete(this.url+'/delete-project/'+projectid,{headers:this.headers})
+   const token = sessionStorage.getItem('UserInfo') ? JSON.parse(sessionStorage.getItem('UserInfo') || '{}').accessToken : '';
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.delete(this.url+'/delete-project/'+projectid,{headers:headers})
   }
 }
